038c21595a4a3db9abedfb78398f03f48078d62f
박지환 <mycook3@naver.com>
2023-10-23 16:32:09 +0900

    [FIRST COMMIT] 스프링 3.15, 자바 17 - String Boot 생성

---
+++ backend/src/main/java/com/a405/gamept/GameptApplication.java
@@ -0,0 +1,13 @@
+package com.a405.gamept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameptApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/a405/gamept/GameptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a405.gamept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
