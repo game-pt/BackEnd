c6dbf7f9d1c9cc2d195a1cadc7c750014d1ee2b4
l0u0h0 <l0u0h0@gmail.com>
2023-10-26 15:37:15 +0900

    :style: tailwind config 작성

---
+++ frontend/gamept/src/oragnisms/DiceModalComponent.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import DiceComponent from "../atoms/DIceComponent";
+
+const DiceModalComponent = () => {
+  const [throwDice, setThrowDice] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('timeout!');
+      setThrowDice(true)
+    }, 3000);
+  })
+
+  return (
+    <div className="card w-full h-96">
+      <h1>주사위 결과</h1>
+      <div className="diceCard">
+        <DiceComponent throw={throwDice} />
+      </div>
+    </div>
+  );
+}
+
+export default DiceModalComponent;
