56f9de2adc2d56753c28aac0d76286c8bdd5a63f
김봉준 <zune1408@gmail.com>
2023-10-27 13:54:34 +0900

    pull 받기

--- frontend/gamept/src/pages/CreateGamePage.tsx
+++ frontend/gamept/src/pages/CreateGamePage.tsx
@@ -1,11 +1,18 @@
 import CardImage from '../atoms/CardImage';
+import ProfileImage from '@/atoms/ProfileImage';
+import Logo from '@/atoms/Logo';
 
-function CreateGamePage() {
+const CreateGamePage = () => {
   return (
     <>
-      <CardImage url="../assets/Leonardo_Diffusion_XL_a_painting_of_one_adventurer_facing_the_1.jpg" />
+      <CardImage
+        url="../assets/Leonardo_Diffusion_XL_a_painting_of_one_adventurer_facing_the_1.jpg"
+        alt=""
+      />
+      <ProfileImage size={160} imgCode={131} alt="" />
+      <Logo />
     </>
   );
-}
+};
 
 export default CreateGamePage;
--- frontend/gamept/vite.config.ts
+++ frontend/gamept/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +9,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: "@components", replacement: "/src/components" },
-      { find: "@", replacement: "/src" },
-    ]
-  }
-})
+      { find: '@components', replacement: '/src/components' },
+      { find: '@', replacement: '/src' },
+    ],
+  },
+});
