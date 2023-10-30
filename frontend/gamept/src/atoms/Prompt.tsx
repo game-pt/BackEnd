b88d0b4aa69cb7adb7b9b11d1a47170c54141447
l0u0h0 <l0u0h0@gmail.com>
2023-10-30 14:44:33 +0900

    :art: [FE] Prompt Atoms 컴포넌트 수정

--- frontend/gamept/src/atoms/Prompt.tsx
+++ frontend/gamept/src/atoms/Prompt.tsx
@@ -8,28 +8,29 @@ import { IPropmpt } from '@/types/components/Prompt.types';
  */
 const Prompt = (props: IPropmpt) => {
   return (
-    <div className="max-w-[1000px] min-w-[20vw] h-[400px] relative bg-transparent">
-      <p
-        className={props.type}
-      >
-        <p>
-          의 내부규율과 사무처리에 관한 규칙을 제정할 수 있다. 법률이 헌법에
-          위반되는 여부가 재판의 전제가 된 경우에는 법원은 헌법재판소에 제청하여
-          그 심판에 의하여 재판한다.
-        </p>
-        <p>
-          이우석은 촌장의 부탁을 받아 숲속에서 사라진 아이들을 찾아 이른 아침 숲
-          속으로 발걸음을 옮기던 중 키가 큰 고블린을 만났다.
-        </p>
-        <p>당신은 어떻게 할 것인가?</p>
-        <p>나 : 싸운다.</p>
-        <p>당신은 어떻게 할 것인가?</p>
-        <p>나 : 싸운다.</p>
-        <p>당신은 어떻게 할 것인가?</p>
-        <p>나 : 싸운다.</p>
-        <p>당신은 어떻게 할 것인가?</p>
-        <p>나 : 싸운다.</p>
-      </p>
+    <div className="max-w-[1000px] min-w-[700px] h-[350px] relative bg-transparent">
+      {props.type && (
+        <div className={props.type}>
+          <p>
+            의 내부규율과 사무처리에 관한 규칙을 제정할 수 있다. 법률이 헌법에
+            위반되는 여부가 재판의 전제가 된 경우에는 법원은 헌법재판소에
+            제청하여 그 심판에 의하여 재판한다.
+          </p>
+          <p>
+            이우석은 촌장의 부탁을 받아 숲속에서 사라진 아이들을 찾아 이른 아침
+            숲 속으로 발걸음을 옮기던 중 키가 큰 고블린을 만났다.
+          </p>
+          <p>당신은 어떻게 할 것인가?</p>
+          <p>나 : 싸운다.</p>
+          <p>당신은 어떻게 할 것인가?</p>
+          <p>나 : 싸운다.</p>
+          <p>당신은 어떻게 할 것인가?</p>
+          <p>나 : 싸운다.</p>
+          <p>당신은 어떻게 할 것인가?</p>
+          <p>나 : 싸운다.</p>
+        </div>
+      )}
+
       <div className="w-full h-1/6 absolute bg-gradient-to-t from-transparent from-30% to-backgroud"></div>
     </div>
   );
