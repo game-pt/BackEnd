8a046ac02b071703c588b588f41e86c4937d2ad8
You Young <dbdud1999@naver.com>
2023-10-30 15:32:48 +0900

    :art: [BE] 에러 처리 방식 Enum으로 변경

---
+++ backend/src/main/java/com/a405/gamept/game/util/enums/GameErrorMessage.java
@@ -0,0 +1,18 @@
+package com.a405.gamept.game.util.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@RequiredArgsConstructor
+public enum GameErrorMessage  {
+    STORY_NOT_FOUND(BAD_REQUEST, "스토리가 존재하지 않습니다."),
+    MONSTER_INVALID(INTERNAL_SERVER_ERROR, "몬스터가 유효하지 않습니다."),
+    RACE_INVALID(INTERNAL_SERVER_ERROR, "종족이 유효하지 않습니다."),
+    STAT_INVALID(INTERNAL_SERVER_ERROR, "스탯이 유효하지 않습니다.");
+
+    private final HttpStatus code;
+    private final String message;
+}
--- backend/src/main/java/com/a405/gamept/global/error/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/a405/gamept/global/error/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.a405.gamept.global.error.exception;
 
+import com.a405.gamept.game.util.exception.GameException;
 import com.a405.gamept.global.error.dto.ErrorResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,21 +20,14 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponseDto> methodArgumentNotValidExceptionHandle(MethodArgumentNotValidException e) {
-        log.error("methodArgumentNotValidException : {}", e);
-        return ResponseEntity.badRequest()
-                .body(ErrorResponseDto.of(HttpStatus.BAD_REQUEST.value(), e.getFieldErrors().get(0).getDefaultMessage()));
+    protected ResponseEntity<?> methodArgumentNotValidExceptionHandle(MethodArgumentNotValidException e) {
+        log.error("methodArgumentNotValidException : {}", e.getFieldErrors().get(0).getDefaultMessage());
+        return new ResponseEntity<>(e.getFieldErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(BadRequestException.class)
-    protected ResponseEntity<ErrorResponseDto> methodArgumentNotValidExceptionHandle(BadRequestException e) {
-        return ResponseEntity.badRequest()
-                .body(ErrorResponseDto.of(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
-    }
-
-    @ExceptionHandler(InternalServerException.class)
-    protected ResponseEntity<ErrorResponseDto> methodArgumentNotValidExceptionHandle(InternalServerException e) {
-        return ResponseEntity.internalServerError()
-                .body(ErrorResponseDto.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 오류"));
+    @ExceptionHandler(GameException.class)
+    protected ResponseEntity<?> methodArgumentNotValidExceptionHandle(GameException e) {
+        log.error("GameException : {}", e.getMessage());
+        return new ResponseEntity<>((e.getCode() == HttpStatus.INTERNAL_SERVER_ERROR)? "서버 오류" : e.getMessage(), e.getCode());
     }
 }
