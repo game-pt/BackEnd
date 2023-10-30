c8f4c236e022db9f538da311afbf650c1c6feb03
You Young <dbdud1999@naver.com>
2023-10-30 13:07:13 +0900

    :art: [BE] 종족 리스트 조회 DTO 생성 및 수정

--- backend/src/main/java/com/a405/gamept/game/dto/command/RaceGetCommandDto.java
+++ backend/src/main/java/com/a405/gamept/game/dto/command/RaceGetCommandDto.java
@@ -1,5 +1,6 @@
 package com.a405.gamept.game.dto.command;
 
+import com.a405.gamept.game.dto.request.RaceGetRequestDto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -8,5 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 @Builder(access = AccessLevel.PRIVATE)
 @Slf4j
 public record RaceGetCommandDto(
-        @NotBlank /*@Size(7)*/ String storyCode
-) { }
+        @NotBlank(message = "스토리가 입력되지 않았습니다.") /*@Size(7)*/ String storyCode
+) {
+    public static RaceGetCommandDto from(RaceGetRequestDto raceGetRequestDto) {
+        return RaceGetCommandDto.builder()
+                .storyCode(raceGetRequestDto.storyCode())
+                .build();
+    }
+}
--- backend/src/main/java/com/a405/gamept/game/dto/response/RaceGetResponseDto.java
+++ backend/src/main/java/com/a405/gamept/game/dto/response/RaceGetResponseDto.java
@@ -6,8 +6,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +17,9 @@ import lombok.extern.slf4j.Slf4j;
 @Builder(access = AccessLevel.PRIVATE)
 @Slf4j
 public record RaceGetResponseDto(
-        @NotBlank /*@Size(7)*/ String code,
-        @NotBlank @Size(min=2, max=10) String name,
-        @Valid @Min(1) List<RaceStatGetResponseDto> statList
+        @NotBlank(message = "종족 코드가 올바르지 않습니다.") /*@Size(7)*/ String code,
+        @NotBlank(message = "종족 이름이 올바르지 않습니다.") @Size(min = 2, max = 10, message = "종족의 이름은 2글자 이상, 10글자 이하여야 합니다.") String name,
+        @Valid @Size(min = 1, message = "스탯이 최소 한 개 이상 존재해야 합니다.") List<RaceStatGetResponseDto> statList
 ) {
     public static RaceGetResponseDto from(Race race) {
         List<RaceStat> statList = race.getRaceStatList();
