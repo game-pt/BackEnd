9159b99fde233f4d351f7d8a9e132e0b1779676a
You Young <dbdud1999@naver.com>
2023-10-30 15:34:52 +0900

    :art: [BE] List 반환 방식 Optional로 변경

--- backend/src/main/java/com/a405/gamept/game/repository/RaceRepository.java
+++ backend/src/main/java/com/a405/gamept/game/repository/RaceRepository.java
@@ -3,11 +3,13 @@ package com.a405.gamept.game.repository;
 import com.a405.gamept.game.entity.Race;
 import com.a405.gamept.game.entity.Story;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RaceRepository extends JpaRepository<Race, String> {
-    List<Race> findAllByStory(Story story) throws DataAccessException;
+    Optional<List<Race>> findAllByStory(Story story) throws DataAccessException;
 }
