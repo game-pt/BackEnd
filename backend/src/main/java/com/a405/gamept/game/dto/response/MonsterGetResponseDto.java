cc8b6bc2c18f7110a68f0b748dcacff6d88fbb15
You Young <dbdud1999@naver.com>
2023-10-30 13:10:43 +0900

    :art: [BE] 레코드 형식 변경에 따른 수정사항 반영

--- backend/src/main/java/com/a405/gamept/game/dto/response/MonsterGetResponseDto.java
+++ backend/src/main/java/com/a405/gamept/game/dto/response/MonsterGetResponseDto.java
@@ -1,5 +1,6 @@
 package com.a405.gamept.game.dto.response;
 
+import com.a405.gamept.game.entity.Monster;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
@@ -9,6 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 @Builder(access = AccessLevel.PRIVATE)
 @Slf4j
 public record MonsterGetResponseDto(
-        @NotBlank String name,
-        @Positive @Max(10) int level
-) { }
+        @NotBlank(message = "몬스터 이름이 올바르지 않습니다.") String name,
+        @Positive(message = "몬스터 레벨은 양수여야 합니다.") @Max(value = 10, message = "몬스터 레벨은 10을 넘을 수 없습니다.") int level
+) {
+    public static MonsterGetResponseDto from(Monster monster) {
+        return MonsterGetResponseDto.builder()
+                .name(monster.getName())
+                .level(monster.getLevel())
+                .build();
+    }
+}
