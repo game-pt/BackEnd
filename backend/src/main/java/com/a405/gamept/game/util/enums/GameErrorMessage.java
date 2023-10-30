93ebb67d40c388bf0363c69e6fc6418724ebb5a7
You Young <dbdud1999@naver.com>
2023-10-30 17:26:02 +0900

    :lock: [BE] 직업 조회 오류 에러 추가

--- backend/src/main/java/com/a405/gamept/game/util/enums/GameErrorMessage.java
+++ backend/src/main/java/com/a405/gamept/game/util/enums/GameErrorMessage.java
@@ -11,6 +11,7 @@ public enum GameErrorMessage  {
     STORY_NOT_FOUND(BAD_REQUEST, "스토리가 존재하지 않습니다."),
     MONSTER_INVALID(INTERNAL_SERVER_ERROR, "몬스터가 유효하지 않습니다."),
     RACE_INVALID(INTERNAL_SERVER_ERROR, "종족이 유효하지 않습니다."),
+    JOB_INVALID(INTERNAL_SERVER_ERROR, "직업이 유효하지 않습니다."),
     STAT_INVALID(INTERNAL_SERVER_ERROR, "스탯이 유효하지 않습니다.");
 
     private final HttpStatus code;
