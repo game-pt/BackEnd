b137d8761567d17c2d1e9168beceb629fc617dd4
l0u0h0 <l0u0h0@gmail.com>
2023-10-31 14:37:39 +0900

    :sparkles: [FE] Prompt 추가 및 글씨 조정

--- frontend/gamept/src/pages/SinglePlayPage.tsx
+++ frontend/gamept/src/pages/SinglePlayPage.tsx
@@ -7,7 +7,7 @@ import PromptInterface from '@/organisms/PromptInterface';
 
 const SinglePlayPage = () => {
   return (
-    <div className="w-[1536px] h-full flex font-hol bg-backgroundDeep text-primary">
+    <div className="w-[1536px] h-screen flex font-hol bg-backgroundDeep text-primary">
       <div className="h-full flex flex-col justify-between items-center">
         <img src={Logo} alt="로고" className="w-[300px]" />
         <SideInterface />
@@ -16,7 +16,7 @@ const SinglePlayPage = () => {
           className="bottom-0 profile-interface w-[400px]"
         />
       </div>
-      <div className="w-full">
+      <div className="w-full h-full">
         <PromptInterface />
       </div>
     </div>
