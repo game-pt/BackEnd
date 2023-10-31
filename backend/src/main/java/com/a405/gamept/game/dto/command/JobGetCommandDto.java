95f433546513381af9fa883b7e4950620c3418e4
You Young <dbdud1999@naver.com>
2023-10-31 15:05:28 +0900

    :art: [BE] 요청 파라미터 Game Code로 변경

--- backend/src/main/java/com/a405/gamept/game/dto/command/JobGetCommandDto.java
+++ backend/src/main/java/com/a405/gamept/game/dto/command/JobGetCommandDto.java
@@ -1,6 +1,7 @@
 package com.a405.gamept.game.dto.command;
 
 import com.a405.gamept.game.dto.request.JobGetRequestDto;
+import com.a405.gamept.play.entity.Game;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,11 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 @Builder(access = AccessLevel.PRIVATE)
 @Slf4j
 public record JobGetCommandDto(
-        @NotBlank(message = "스토리가 입력되지 않았습니다.") /*@Size(7)*/ String storyCode
+        @NotBlank(message = "게임이 존재하지 않습니다.") String gameCode
 ) {
     public static JobGetCommandDto from(JobGetRequestDto jobGetRequestDto) {
         return JobGetCommandDto.builder()
-                .storyCode(jobGetRequestDto.storyCode())
+                .gameCode(jobGetRequestDto.gameCode())
                 .build();
     }
 }
--- backend/src/main/java/com/a405/gamept/game/dto/request/JobGetRequestDto.java
+++ backend/src/main/java/com/a405/gamept/game/dto/request/JobGetRequestDto.java
@@ -1,5 +1,6 @@
 package com.a405.gamept.game.dto.request;
 
+import com.a405.gamept.play.entity.Game;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -8,5 +9,5 @@ import lombok.extern.slf4j.Slf4j;
 @Builder(access = AccessLevel.PRIVATE)
 @Slf4j
 public record JobGetRequestDto(
-        @NotBlank(message = "스토리가 입력되지 않았습니다.") /*@Size(7)*/ String storyCode
+        @NotBlank(message = "게임이 존재하지 않습니다.") String gameCode
 ) { }
