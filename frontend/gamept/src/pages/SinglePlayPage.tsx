95f4a4d218621bc23e32f308ddf12a759a9ec787
l0u0h0 <l0u0h0@gmail.com>
2023-10-31 16:22:42 +0900

    :lipstick: [FE] 게임플레이 UI 수정

--- frontend/gamept/src/pages/SinglePlayPage.tsx
+++ frontend/gamept/src/pages/SinglePlayPage.tsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import Logo from '@/assets/logo/logo.png';
+import Logo from '@/assets/GamePTLogo.svg';
 import ProfileInterface from '@/assets/interface/profile_interface.png';
 import SideInterface from '@/organisms/SideInterface';
 import PromptInterface from '@/organisms/PromptInterface';
@@ -9,17 +9,22 @@ import TextButton from '@/atoms/TextButton';
 const SinglePlayPage = () => {
   return (
     <div className="w-screen h-screen flex font-hol bg-backgroundDeep text-primary">
-      <div className="h-full flex flex-col justify-between items-center">
-        <img src={Logo} alt="로고" className="w-[300px]" />
-        <SideInterface />
+      <div className="basis-1/3 h-full flex flex-col justify-between items-start">
+        <img src={Logo} alt="로고" className="w-[300px] m-4 ml-6" />
+        <div className="w-full flex justify-center">
+          <SideInterface />
+        </div>
         <img
           src={ProfileInterface}
-          className="bottom-0 profile-interface w-[400px]"
+          className="bottom-0 profile-interface w-[450px]"
         />
       </div>
-      <div className="w-full h-full mx-4">
+      <div className="basis-3/4 h-full mr-2">
         <div className="w-full flex justify-end py-1 pr-10">
-          <TextButton text={`게임 나가기`} onClickEvent={() => console.log('진짜 나가요?')} />
+          <TextButton
+            text={`게임 나가기`}
+            onClickEvent={() => console.log('진짜 나가요?')}
+          />
         </div>
         <PromptInterface />
       </div>
