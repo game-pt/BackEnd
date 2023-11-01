0a0c8e9643d2b28d75e34d0fa551f83390475fe9
김봉준 <zune1408@gmail.com>
2023-11-01 14:30:58 +0900

    :sparkles: [FE] 이어하기 모달 구현 - 로직은 미구현

--- frontend/gamept/src/organisms/MultiplayModal.tsx
+++ frontend/gamept/src/organisms/MultiplayModal.tsx
@@ -10,7 +10,7 @@ import Input from '@/atoms/Input';
 
 const MultiplayModal = () => {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-hol w-[590px] border-[3px] pt-10 border-primary h-[470px] rounded-[10px] bg-backgroundDeep/80">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1f/2 font-hol w-[590px] border-[3px] pt-10 border-primary h-[470px] rounded-[10px] bg-backgroundDeep/80">
       <div className="flex flex-col items-center gap-10">
         <div className=" text-primary text-28 mb-10">멀티 플레이</div>
         <SelectButton
