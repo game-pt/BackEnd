09ef62f18e6fc72bcf124f8ef1039e8c6342584e
You Young <dbdud1999@naver.com>
2023-11-01 14:23:35 +0900

    :sparkles: [BE] 스토리 리스트 조회 API 구현

--- backend/src/main/java/com/a405/gamept/game/controller/GameController.java
+++ backend/src/main/java/com/a405/gamept/game/controller/GameController.java
@@ -1,9 +1,22 @@
 package com.a405.gamept.game.controller;
 
+import com.a405.gamept.game.service.GameService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("game")
 public class GameController {
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping("story")
+    public ResponseEntity<?> getStoryList() {
+        return ResponseEntity.ok(gameService.getStoryList());
+    }
 }
