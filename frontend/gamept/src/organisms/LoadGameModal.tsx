abd7f9c56d002a557ae5c8b2ca18afb5df8a1398
김봉준 <zune1408@gmail.com>
2023-11-01 14:33:12 +0900

    :lipstick: [FE] 모달 position absoulte화 및 가운데 정렬

--- frontend/gamept/src/organisms/LoadGameModal.tsx
+++ frontend/gamept/src/organisms/LoadGameModal.tsx
@@ -8,7 +8,7 @@ import SelectButton from '@/atoms/SelectButton';
 
 const LoadGameModal = () => {
   return (
-    <div className="w-[590px] h-[470px] py-20 bg-backgroundDeep/80 border-primary text-black border-[3px] rounded-[10px] font-hol">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[590px] h-[470px] py-20 bg-backgroundDeep/80 border-primary text-black border-[3px] rounded-[10px] font-hol">
       <div className="text-primary text-[36px] mb-10">게임 이어하기</div>
       <div className="text-primary text-28 mb-10">
         이전에 했던 플레이의 기록이 있습니다.
