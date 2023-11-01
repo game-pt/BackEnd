6e6e18eedc3a715436bcc4dc1df83c775edc2f3e
l0u0h0 <l0u0h0@gmail.com>
2023-11-01 16:47:30 +0900

    :rewind: [FE] 의존성 제거 (없어도 잘되네요,,!)

--- frontend/gamept/vite.config.ts
+++ frontend/gamept/vite.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  base: './',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   resolve: {
     alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@', replacement: '/src' },
     ],
   },
 });
