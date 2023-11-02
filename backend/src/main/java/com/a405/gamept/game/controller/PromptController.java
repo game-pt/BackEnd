29ecca7225583aab423fb04571b80de919693a31
dldntjr0516@gmail.com <dldntjr0516@gmail.com>
2023-11-02 12:40:05 +0900

    :sparkles: [BE/feat] Event 랜덤 발생 로직 구현 완료 (Event 관련 로직 모두 완료) - 주석 추가

--- backend/src/main/java/com/a405/gamept/game/controller/PromptController.java
+++ backend/src/main/java/com/a405/gamept/game/controller/PromptController.java
@@ -3,10 +3,12 @@ package com.a405.gamept.game.controller;
 import com.a405.gamept.game.dto.command.GetPromptResultCommandDto;
 import com.a405.gamept.game.dto.command.RaceGetCommandDto;
 import com.a405.gamept.game.dto.request.GetPromptResultRequestDto;
+import com.a405.gamept.game.dto.response.GetPromptResultResponseDto;
 import com.a405.gamept.game.service.PromptService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +19,8 @@ public class PromptController {
 
     private final PromptService promptService;
 
-    public ResponseEntity<?> getPromptResult(@Valid GetPromptResultRequestDto getPromptResultRequestDto) {
+    @GetMapping
+    public ResponseEntity<GetPromptResultResponseDto> getPromptResult(@Valid GetPromptResultRequestDto getPromptResultRequestDto) {
         return ResponseEntity.ok(promptService.getPrmoptResult(GetPromptResultCommandDto.from(getPromptResultRequestDto)));
     }
 
