581934729e29f50844bde4d670237c267a5fc150
hehezune <funcodezune@gmail.com>
2023-11-06 23:10:51 +0900

    :sparkles: [FE] CreateCharacterPage 조립 및 라우터 등록

--- frontend/gamept/src/main.tsx
+++ frontend/gamept/src/main.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import CreateGamePage from './pages/CreateGamePage.tsx';
 import SinglePlayPage from './pages/SinglePlayPage.tsx'; // 새로 만든 페이지 컴포넌트를 불러옵니다.
-
+import CreateCharacterPage from './pages/CreateCharacterPage.tsx';
 
 // 라우팅 경로 지정
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/createGame', element: <CreateGamePage /> },
   { path: '/singleplay', element: <SinglePlayPage /> }, // 새로운 경로 추가
-
-])
+  { path: '/createCharacter', element: <CreateCharacterPage /> }, // 새로운 경로 추가
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ frontend/gamept/src/pages/CreateCharacterPage.tsx
@@ -0,0 +1,63 @@
+/**
+ *
+ * @returns
+ */
+
+import Logo from '@/atoms/Logo';
+import SelectCharacter from '@/organisms/SelectCharacter';
+import MakeCharacterName from '@/organisms/MakeCharacterName';
+import { useState } from 'react';
+import { ICharacterStatus } from '@/types/components/MakeGameProcess.type';
+import { useNavigate } from 'react-router-dom';
+// import { useAtom } from "jotai"
+
+const CreateCharacterPage = () => {
+  const [characterStatus, setCharacterStatus] = useState<ICharacterStatus>({
+    race: '',
+    className: '',
+    gender: 0,
+    name: '',
+  });
+
+  const [processLevel, setProcessLevel] = useState(0);
+  const navigate = useNavigate();
+  const handleNextLevel = () => setProcessLevel(processLevel + 1);
+  const handlePreviousLevel = () => setProcessLevel(processLevel - 1);
+  const handleRaceSelect = (gender: number, race: string) => {
+    setCharacterStatus({ ...characterStatus, gender, race });
+  };
+  const handleClassSelect = (gender: number, className: string) => {
+    setCharacterStatus({ ...characterStatus, gender, className });
+  };
+  const createCharacterProcess = [
+    <SelectCharacter
+      apiURL={gameID}
+      type="종족"
+      onNextLevel={handleNextLevel}
+      gender={characterStatus.gender}
+      onSetCharacter={handleRaceSelect}
+      onPreviosLevel={() => navigate('/createGame')}
+    />,
+    <SelectCharacter
+      apiURL={gameID}
+      type="직업"
+      onNextLevel={handleNextLevel}
+      gender={characterStatus.gender}
+      onPreviosLevel={handlePreviousLevel}
+      onSetCharacter={handleClassSelect}
+    />,
+    <MakeCharacterName
+      characterStatus={characterStatus}
+      setCharacterName={setCharacterStatus}
+    />,
+  ];
+  return (
+    <div>
+      <Logo />
+      {createCharacterProcess[processLevel]}
+    </div>
+  );
+};
+
+const gameID = '';
+export default CreateCharacterPage;
