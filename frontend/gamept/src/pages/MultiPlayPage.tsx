de388802bc0130ab3a0c4f796e4d70e005682264
l0u0h0 <l0u0h0@gmail.com>
2023-11-07 15:58:13 +0900

    :lipstick: [FE] 싱글 멀티 페이지 UI 맞추기

--- frontend/gamept/src/pages/MultiPlayPage.tsx
+++ frontend/gamept/src/pages/MultiPlayPage.tsx
@@ -1,14 +1,14 @@
 import SockJS from 'sockjs-client';
 import { CompatClient, Stomp } from '@stomp/stompjs';
 import { useState, useRef, useEffect } from 'react';
-import Logo from '@/assets/GamePTLogo.svg';
+import Logo from '@/assets/logo/logo.png';
 import SideInterface from '@/organisms/SideInterface';
 import PromptInterface from '@/organisms/PromptInterface';
 import TextButton from '@/atoms/TextButton';
 import ProfileInterface from '@/organisms/ProfileInterface';
 import { getPromptTopic } from '@/services/GameService';
 import { useIndexedDB } from 'react-indexed-db-hook';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
 
 const MultiPlayPage = () => {
@@ -17,7 +17,7 @@ const MultiPlayPage = () => {
   const client = useRef<CompatClient | null>(null);
   const gameCode = 'YMi8mg';
   const playerCode = 'YMi8mg-yl7q7k';
-  const db = useIndexedDB("prompt");
+  const db = useIndexedDB('prompt');
   const navigate = useNavigate();
 
   // 웹소캣 객체 생성
@@ -33,15 +33,19 @@ const MultiPlayPage = () => {
         console.log('Error');
         return;
       }
-      
+
       // 멀티플레이 용
       // 유저 데이터 업데이트 시 정보 송수신용
-      client.current.subscribe(`/topic/player/${gameCode}`, (message) => {
-        console.log(JSON.parse(message.body));
-        // message.body를 통해 데이터 받아서
-        // 프로필 업데이트 로직 수행
-        // 해당 방 구독하는 모든 플레이어들 데이터 저장하는 객체에다가 업데이트
-      }, {});
+      client.current.subscribe(
+        `/topic/player/${gameCode}`,
+        (message) => {
+          console.log(JSON.parse(message.body));
+          // message.body를 통해 데이터 받아서
+          // 프로필 업데이트 로직 수행
+          // 해당 방 구독하는 모든 플레이어들 데이터 저장하는 객체에다가 업데이트
+        },
+        {}
+      );
 
       // 연결 성공 시 해당 방을 구독하면 서버로부터 이벤트 발생 & 프롬프트 추가 시 마다 메세지 수신
       client.current.subscribe(
@@ -55,7 +59,6 @@ const MultiPlayPage = () => {
             console.log(origin);
             // 인덱스 디비에 프롬프트 데이터 저장.
             db.add({ content: prompt });
-
           }
 
           // 기존 프롬프트 내역에 새로운 메시지 추가
@@ -129,31 +132,31 @@ const MultiPlayPage = () => {
 
   const leaveGame = () => {
     Swal.fire({
-      title: "경고문",
-      text: "이 게임 정보가 삭제됩니다람쥐!",
+      title: '경고문',
+      text: '이 게임 정보가 삭제됩니다람쥐!',
       width: 600,
-      padding: "2rem",
-      color: "#FBCB73",
-      background: "#240903",
+      padding: '2rem',
+      color: '#FBCB73',
+      background: '#240903',
       showDenyButton: true,
-      confirmButtonText: "머무르기",
-      confirmButtonColor: "#F0B279",
-      denyButtonText: "나가기",
-      denyButtonColor: "#C9C9C9",
+      confirmButtonText: '머무르기',
+      confirmButtonColor: '#F0B279',
+      denyButtonText: '나가기',
+      denyButtonColor: '#C9C9C9',
       customClass: {
-        container: "font-hol",
-        popup: "rounded-lg",
-        title: "text-48 mb-8",
-        htmlContainer: "text-36",
+        container: 'font-hol',
+        popup: 'rounded-lg',
+        title: 'text-48 mb-8',
+        htmlContainer: 'text-36',
       },
-    }).then(result => {
+    }).then((result) => {
       if (result.isDenied) {
         disConnected();
         db.clear();
-        navigate("/");
+        navigate('/');
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     if (client.current === null) {
@@ -169,8 +172,8 @@ const MultiPlayPage = () => {
 
   return (
     <div className="w-screen h-screen flex font-hol bg-backgroundDeep text-primary">
-      <div className="basis-1/3 h-full flex flex-col justify-between items-start">
-        <img src={Logo} alt="로고" className="w-[200px] m-4 ml-6" />
+      <div className="w-400 h-full flex flex-col justify-between items-start">
+        <img src={Logo} alt="로고" className="w-[300px]" />
         <div className="w-full h-[400px] flex justify-center">
           <SideInterface sendChat={sendChatHandler} chat={chat} />
         </div>
