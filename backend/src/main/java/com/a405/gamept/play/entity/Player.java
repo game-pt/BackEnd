57ebe4b854048a6f7e5b436800079a96d411c864
You Young <dbdud1999@naver.com>
2023-11-08 10:17:52 +0900

    :bug: [BE] Redis에 맞는 변수 타입 변경

--- backend/src/main/java/com/a405/gamept/play/entity/Player.java
+++ backend/src/main/java/com/a405/gamept/play/entity/Player.java
@@ -48,12 +48,12 @@ public class Player {
     /**
      * itemList : 사용자가 가지고 있는 아이템 목록
      */
-    private List<Item> itemList;
+    private List<String> itemCodeList;
 
     /**
      * newItem : 새로운 아이템, 사용자가 획득을 원할 시 습득할 아이템이다.
      */
-    private Item newItem;
+    private String newItemCode;
     
     /**
      * nickname : 사용자의 닉네임
