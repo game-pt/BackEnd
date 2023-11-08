242254a51b15119649c2035586c891c981815987
You Young <dbdud1999@naver.com>
2023-11-08 15:05:37 +0900

    :art: [BE] 상수 데이터 변경에 따른 수정

--- backend/src/main/java/com/a405/gamept/game/service/FightServiceImpl.java
+++ backend/src/main/java/com/a405/gamept/game/service/FightServiceImpl.java
@@ -6,7 +6,7 @@ import com.a405.gamept.game.entity.Monster;
 import com.a405.gamept.game.entity.Story;
 import com.a405.gamept.game.repository.MonsterRepository;
 import com.a405.gamept.game.repository.StoryRepository;
-import com.a405.gamept.game.util.FinalData;
+import com.a405.gamept.game.util.GameData;
 import com.a405.gamept.game.util.enums.GameErrorMessage;
 import com.a405.gamept.game.util.exception.GameException;
 import jakarta.validation.ConstraintViolation;
@@ -37,7 +37,7 @@ public class FightServiceImpl implements FightService {
 
         /** 몬스터 레벨 난수 뽑기 **/
         int sum = 0;  // 랜덤하게 돌릴 숫자
-        for(int i : FinalData.monsterRate.values()) {
+        for(int i : GameData.monsterRate.values()) {
             sum += i;  // 각 확률 더하기
         }
 
@@ -45,8 +45,8 @@ public class FightServiceImpl implements FightService {
         int randNum = (int) Math.floor(Math.random() * sum);  // 몬스터 레벨 확률
 
         sum = 0;  // 각 확률 더하기
-        for(int key : FinalData.monsterRate.keySet()) {
-            sum += FinalData.monsterRate.get(key);  // 확률 더하기
+        for(int key : GameData.monsterRate.keySet()) {
+            sum += GameData.monsterRate.get(key);  // 확률 더하기
             if(randNum < sum) {  // 확률에 해당할 경우
                 int monsterLevel = monsterGetCommandDto.playerLevel() + key;
                 if(monsterLevel <= 0){  // 몬스터 레벨이 0 이하일 경우
