56d2946731a60b3f9ea3c99ff5d9372d8b41954e
You Young <dbdud1999@naver.com>
2023-11-08 10:20:53 +0900

    :fire: [BE] 미사용 import문 삭제

--- backend/src/main/java/com/a405/gamept/play/repository/PlayerRedisRepository.java
+++ backend/src/main/java/com/a405/gamept/play/repository/PlayerRedisRepository.java
@@ -1,7 +1,6 @@
 package com.a405.gamept.play.repository;
 
 import com.a405.gamept.play.entity.Player;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
