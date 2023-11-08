ec39b6d932a7e604fbe7101b815f076959c39f6b
You Young <dbdud1999@naver.com>
2023-11-08 09:58:43 +0900

    :sparkles: [BE] 아이템 추가 API 구현

---
+++ backend/src/main/java/com/a405/gamept/game/service/ItemService.java
@@ -0,0 +1,19 @@
+package com.a405.gamept.game.service;
+
+import com.a405.gamept.game.dto.command.ItemGetCommandDto;
+import com.a405.gamept.game.dto.command.ItemSetCommandDto;
+import com.a405.gamept.game.dto.response.ItemGetResponseDto;
+import com.a405.gamept.game.dto.response.ItemSetResponseDto;
+import com.a405.gamept.game.util.exception.GameException;
+
+import java.util.List;
+
+public interface ItemService {
+    /**
+     * 아이템을 업데이트 한다.
+     * @param itemSetCommandDto : 게임코드, 플레이어 코드
+     * @return : 아이템을 해당 플레이어에게 응답
+     * @author : 유영
+     */
+    ItemSetResponseDto setItem(ItemSetCommandDto itemSetCommandDto) throws GameException;
+}
