87e7e6a12af7844bc15e933be38f383f0d4f1b4c
You Young <dbdud1999@naver.com>
2023-11-10 14:56:52 +0900

    :sparkles: [BE] 임의 코드 생성 로직 UTIL로 빼기

--- backend/src/main/java/com/a405/gamept/util/ValidateUtil.java
+++ backend/src/main/java/com/a405/gamept/util/ValidateUtil.java
@@ -5,11 +5,14 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 
+import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ValidateUtil {
     private static final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-    static Set<ConstraintViolation<Object>> violations;
+    private static Set<ConstraintViolation<Object>> violations;
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     public static void validate(Object object) {
         violations = validator.validate(object);
@@ -19,7 +22,16 @@ public class ValidateUtil {
                 throw new GameException(violation.getMessage());
             }
         }
+    }
 
-        return;
+    public static String getRandomUID() {
+        return getRandomUID(6);  // 기본 길이를 6으로 설정
+    }
+
+    public static String getRandomUID(int streamSize) {
+        return new Random().ints(6, 0, CHARACTERS.length())
+                .mapToObj(CHARACTERS::charAt)
+                .map(Object::toString)
+                .collect(Collectors.joining());
     }
 }
