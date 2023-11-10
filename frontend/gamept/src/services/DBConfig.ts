e8d4ac86518aa1233fb6caac72a911322622339e
김봉준 <zune1408@gmail.com>
2023-11-10 17:46:30 +0900

    :sparkles: [FE] code들을 indexedDB로 관리하기 위한 config 파일 수정

--- frontend/gamept/src/services/DBConfig.ts
+++ frontend/gamept/src/services/DBConfig.ts
@@ -1,12 +1,24 @@
 export const DBConfig = {
-  name: "PromptData",
+  name: 'PromptData',
   version: 1,
   objectStoresMeta: [
     {
-      store: "prompt",
-      storeConfig: { keyPath: "id", autoIncrement: true },
+      store: 'prompt',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
       storeSchema: [
-        { name: "content", keypath: "content", options: { unique: false } },
+        { name: 'content', keypath: 'content', options: { unique: false } },
+      ],
+    },
+    {
+      store: 'codeStore',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'gameCode', keypath: 'gameCode', options: { unique: true } },
+        {
+          name: 'playerCode',
+          keypath: 'playerCode',
+          options: { unique: true },
+        },
       ],
     },
   ],
