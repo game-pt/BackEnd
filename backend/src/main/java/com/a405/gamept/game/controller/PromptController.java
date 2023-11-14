8fdad1f0370f0edfc75070a60eb885f2c4bbb0ac
You Young <dbdud1999@naver.com>
2023-11-14 15:40:29 +0900

    :art: [BE] Prompt 전송 분리

--- backend/src/main/java/com/a405/gamept/game/controller/PromptController.java
+++ backend/src/main/java/com/a405/gamept/game/controller/PromptController.java
@@ -1,10 +1,13 @@
 package com.a405.gamept.game.controller;
 
 import com.a405.gamept.game.dto.command.ChatCommandDto;
+import com.a405.gamept.game.dto.command.PromptListGetCommandDto;
 import com.a405.gamept.game.dto.command.PromptResultGetCommandDto;
 import com.a405.gamept.game.dto.request.ChatRequestDto;
+import com.a405.gamept.game.dto.request.PromptListGetRequestDto;
 import com.a405.gamept.game.dto.request.PromptResultGetRequestDto;
 import com.a405.gamept.game.dto.response.ChatResponseDto;
+import com.a405.gamept.game.dto.response.PromptGetResponseDto;
 import com.a405.gamept.game.dto.response.PromptResultGetResponseDto;
 import com.a405.gamept.game.service.PromptService;
 import com.a405.gamept.game.util.exception.GameException;
@@ -18,6 +21,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("prompt")
 @RequiredArgsConstructor
 public class PromptController {
 
@@ -34,8 +38,17 @@ public class PromptController {
     @Deprecated
     public void getPromptResult(@Valid @Payload PromptResultGetRequestDto promptResultGetRequestDto, @Valid @DestinationVariable String gameCode) {
         PromptResultGetCommandDto promptResultGetCommandDto = PromptResultGetCommandDto.from(promptResultGetRequestDto, gameCode);
-        PromptResultGetResponseDto promptResultGetResponseDto = promptService.getPrmoptResult(promptResultGetCommandDto);
-        webSocket.convertAndSend("/topic/prompt/" + gameCode, promptResultGetResponseDto);
+        // 유저 프롬프트 보내기
+        webSocket.convertAndSend("/topic/prompt/" + gameCode, promptService.setUserPrompt(promptResultGetCommandDto));
+        PromptGetResponseDto promptGetResponseDto = promptService.getChatGPTPrompt(promptResultGetCommandDto);
+        // ChatGPT 프롬프트 보내기
+        webSocket.convertAndSend("/topic/prompt/" + gameCode, promptGetResponseDto);
+        // 이벤트 발생 체크 후 보내기
+        webSocket.convertAndSend("/topic/prompt/" + gameCode, promptService.getPrmoptResult(promptResultGetCommandDto, promptGetResponseDto.content()));
+    }
+    @GetMapping
+    public ResponseEntity<?> getPromptList(@Valid PromptListGetRequestDto promptListGetRequestDto) throws GameException {
+        return ResponseEntity.ok(promptService.getPromptList(PromptListGetCommandDto.from(promptListGetRequestDto)));
     }
 
 }
