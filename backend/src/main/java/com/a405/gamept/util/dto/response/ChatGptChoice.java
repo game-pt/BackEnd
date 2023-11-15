7c7e12e77629e1dce749b2c818ecde4b6f07151a
박지환 <mycook3@naver.com>
2023-11-15 13:24:35 +0900

    Revert "Merge branch 'be/develop' into 'master'"
    This reverts merge request !144

--- backend/src/main/java/com/a405/gamept/util/dto/response/ChatGptChoice.java
+++
@@ -1,9 +0,0 @@
-package com.a405.gamept.util.dto.response;
-
-public record ChatGptChoice(
-        ChatGptChoiceMessage message,
-        int index,
-        Object logprobs,
-        String finish_reason
-) {
-}
--- backend/src/main/java/com/a405/gamept/util/dto/response/ChatGptChoiceMessage.java
+++
@@ -1,7 +0,0 @@
-package com.a405.gamept.util.dto.response;
-
-public record ChatGptChoiceMessage(
-        String role,
-        String content
-) {
-}
